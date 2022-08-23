856ce374d025a519d308f3b2aa0128e866ea8cab
amirhnajafiz <najafizadeh21@gmail.com>
2022-08-23 15:18:37 +0430

    add: handler methods

--- internal/http/handler/handler.go
+++ internal/http/handler/handler.go
@@ -1,7 +1,28 @@
 package handler
 
-import "github.com/amirhnajafiz/distributed-redis/internal/store"
+import (
+	"github.com/amirhnajafiz/distributed-redis/internal/store"
+	"github.com/gin-gonic/gin"
+)
 
 type Handler struct {
 	Store store.Store
+}
+
+func (h *Handler) Insert(c *gin.Context) {
+
+}
+
+func (h *Handler) Delete(c *gin.Context) {
+
+}
+
+func (h *Handler) Get(c *gin.Context) {
+
+}
+
+func (h *Handler) Register(r *gin.RouterGroup) {
+	r.POST("/data", h.Insert)
+	r.DELETE("/data/{key}", h.Delete)
+	r.GET("/data/{key}", h.Get)
 }
