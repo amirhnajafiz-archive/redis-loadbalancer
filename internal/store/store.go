22ae6d3a03c695f45875fdf1b193c6f78ca89962
amirhnajafiz <najafizadeh21@gmail.com>
2022-08-23 09:59:49 +0430

    feat: store

--- internal/store/store.go
+++ internal/store/store.go
@@ -1,1 +1,42 @@
 package store
+
+import (
+	"context"
+
+	"github.com/go-redis/redis/v9"
+)
+
+type Store struct {
+	Conn *redis.Client
+}
+
+func (s *Store) Put(key, value string) error {
+	ctx := context.Background()
+
+	if err := s.Conn.Set(ctx, key, value, 0).Err(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *Store) Trash(key string) error {
+	ctx := context.Background()
+
+	if err := s.Conn.Del(ctx, key).Err(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *Store) Pull(key string) (string, error) {
+	ctx := context.Background()
+
+	value, err := s.Conn.Get(ctx, key).Result()
+	if err != nil {
+		return "", err
+	}
+
+	return value, nil
+}
