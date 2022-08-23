a4e1e2d284eed3a0c5138ba5e5d1789105763242
amirhnajafiz <najafizadeh21@gmail.com>
2022-08-23 09:54:37 +0430

    init: redis

--- internal/store/redis.go
+++ internal/store/redis.go
@@ -1,1 +1,13 @@
 package store
+
+import "github.com/go-redis/redis/v9"
+
+// Connect method makes a connection to
+// redis client.
+func Connect(addr string) *redis.Client {
+	return redis.NewClient(&redis.Options{
+		Addr:     addr,
+		Password: "",
+		DB:       0,
+	})
+}
