b0c7eb760f2b5fce11158af7acfcacb6e1937af9
amirhnajafiz <najafizadeh21@gmail.com>
2022-08-23 15:27:01 +0430

    add: root

--- internal/cmd/root.go
+++ internal/cmd/root.go
@@ -1,1 +1,23 @@
 package cmd
+
+import (
+	"github.com/amirhnajafiz/distributed-redis/internal/http/handler"
+	"github.com/amirhnajafiz/distributed-redis/internal/store"
+	"github.com/gin-gonic/gin"
+)
+
+func Execute() {
+	app := gin.Default()
+
+	h := handler.Handler{
+		Store: store.Store{
+			Conn: store.Connect("localhost:6379"),
+		},
+	}
+
+	h.Register(app.Group("/api"))
+
+	if err := app.Run(":8080"); err != nil {
+		panic(err)
+	}
+}
