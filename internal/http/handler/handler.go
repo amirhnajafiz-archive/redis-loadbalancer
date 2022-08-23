65f5b96ac34d98dd9feebfd2ee08c4e9ac86a7db
amirhnajafiz <najafizadeh21@gmail.com>
2022-08-23 15:21:35 +0430

    add: get handler

--- internal/http/handler/handler.go
+++ internal/http/handler/handler.go
@@ -1,6 +1,10 @@
 package handler
 
 import (
+	"net/http"
+	"time"
+
+	"github.com/amirhnajafiz/distributed-redis/internal/http/response"
 	"github.com/amirhnajafiz/distributed-redis/internal/store"
 	"github.com/gin-gonic/gin"
 )
@@ -18,7 +22,18 @@ func (h *Handler) Delete(c *gin.Context) {
 }
 
 func (h *Handler) Get(c *gin.Context) {
+	value, err := h.Store.Pull(c.Param("key"))
+	if err != nil {
+		_ = c.Error(err)
 
+		return
+	}
+
+	c.JSON(http.StatusOK, response.PairResponse{
+		Key:   c.Param("key"),
+		Value: value,
+		Date:  time.Now(),
+	})
 }
 
 func (h *Handler) Register(r *gin.RouterGroup) {
