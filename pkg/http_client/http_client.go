d70d2779b767de9ccd986d15327ab862a251d131
amirhnajafiz <najafizadeh21@gmail.com>
2022-09-05 09:25:15 +0430

    feat: get in http client

--- pkg/http_client/http_client.go
+++ pkg/http_client/http_client.go
@@ -1,6 +1,9 @@
 package http_client
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 type HTTPClient struct {
 	client *http.Client
@@ -10,4 +13,18 @@ func New() *HTTPClient {
 	return &HTTPClient{
 		client: &http.Client{},
 	}
+}
+
+func (h HTTPClient) Get(uri string) (*http.Response, error) {
+	req, err := http.NewRequest(http.MethodGet, uri, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed in creating requests: %w", err)
+	}
+
+	resp, err := h.client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("http request failed: %w", err)
+	}
+
+	return resp, nil
 }
