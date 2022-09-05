f46964042b8707540a6e67ba36626dc9572fb5f8
amirhnajafiz <najafizadeh21@gmail.com>
2022-09-05 09:26:38 +0430

    feat: post request in http client

--- pkg/http_client/http_client.go
+++ pkg/http_client/http_client.go
@@ -2,6 +2,7 @@ package http_client
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 )
 
@@ -17,6 +18,20 @@ func New() *HTTPClient {
 
 func (h HTTPClient) Get(uri string) (*http.Response, error) {
 	req, err := http.NewRequest(http.MethodGet, uri, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed in creating requests: %w", err)
+	}
+
+	resp, err := h.client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("http request failed: %w", err)
+	}
+
+	return resp, nil
+}
+
+func (h HTTPClient) Post(uri string, body io.Reader) (*http.Response, error) {
+	req, err := http.NewRequest(http.MethodPost, uri, body)
 	if err != nil {
 		return nil, fmt.Errorf("failed in creating requests: %w", err)
 	}
