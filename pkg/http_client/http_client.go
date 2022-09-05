1a00f709a028a341aff022e1c1bdfbdf01425f40
amirhnajafiz <najafizadeh21@gmail.com>
2022-09-05 09:42:47 +0430

    add: headers into http client methods

--- pkg/http_client/http_client.go
+++ pkg/http_client/http_client.go
@@ -4,24 +4,37 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strings"
 )
 
+// HTTPClient
+// manages to make http calls.
 type HTTPClient struct {
 	client *http.Client
 }
 
+// New
+// creating a new http client.
 func New() *HTTPClient {
 	return &HTTPClient{
 		client: &http.Client{},
 	}
 }
 
-func (h HTTPClient) Get(uri string) (*http.Response, error) {
+// Get
+// making a get request.
+func (h HTTPClient) Get(uri string, headers ...string) (*http.Response, error) {
 	req, err := http.NewRequest(http.MethodGet, uri, nil)
 	if err != nil {
 		return nil, fmt.Errorf("failed in creating requests: %w", err)
 	}
 
+	for _, pair := range headers {
+		parts := strings.Split(pair, ":")
+
+		req.Header.Add(parts[0], parts[1])
+	}
+
 	resp, err := h.client.Do(req)
 	if err != nil {
 		return nil, fmt.Errorf("http request failed: %w", err)
@@ -30,13 +43,19 @@ func (h HTTPClient) Get(uri string) (*http.Response, error) {
 	return resp, nil
 }
 
-func (h HTTPClient) Post(uri string, body io.Reader, content string) (*http.Response, error) {
+// Post
+// making a post request.
+func (h HTTPClient) Post(uri string, body io.Reader, headers ...string) (*http.Response, error) {
 	req, err := http.NewRequest(http.MethodPost, uri, body)
 	if err != nil {
 		return nil, fmt.Errorf("failed in creating requests: %w", err)
 	}
 
-	req.Header.Add("content-type", content)
+	for _, pair := range headers {
+		parts := strings.Split(pair, ":")
+
+		req.Header.Add(parts[0], parts[1])
+	}
 
 	resp, err := h.client.Do(req)
 	if err != nil {
@@ -46,10 +65,18 @@ func (h HTTPClient) Post(uri string, body io.Reader, content string) (*http.Resp
 	return resp, nil
 }
 
-func (h HTTPClient) Delete(uri string) (*http.Response, error) {
+// Delete
+// making a delete request.
+func (h HTTPClient) Delete(uri string, headers ...string) (*http.Response, error) {
 	req, err := http.NewRequest(http.MethodDelete, uri, nil)
 	if err != nil {
 		return nil, fmt.Errorf("failed in creating requests: %w", err)
+	}
+
+	for _, pair := range headers {
+		parts := strings.Split(pair, ":")
+
+		req.Header.Add(parts[0], parts[1])
 	}
 
 	resp, err := h.client.Do(req)
