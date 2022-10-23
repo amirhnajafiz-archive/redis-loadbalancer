aa9a69a773afa9c2ec11f11467056337c486e001
amirhnajafiz <najafizadeh21@gmail.com>
2022-10-23 11:25:23 +0330

    add: strago in cluster new method

--- go.mod
+++ go.mod
@@ -8,6 +8,8 @@ require (
 )
 
 require (
+	github.com/amirhnajafiz/strago v0.0.7 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
@@ -15,13 +17,22 @@ require (
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.11.1 // indirect
 	github.com/goccy/go-json v0.9.11 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.2 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.5 // indirect
+	github.com/prometheus/client_golang v1.13.0 // indirect
+	github.com/prometheus/client_model v0.3.0 // indirect
+	github.com/prometheus/common v0.37.0 // indirect
+	github.com/prometheus/procfs v0.8.0 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
+	go.uber.org/atomic v1.10.0 // indirect
+	go.uber.org/multierr v1.8.0 // indirect
+	go.uber.org/zap v1.23.0 // indirect
 	golang.org/x/crypto v0.1.0 // indirect
 	golang.org/x/net v0.1.0 // indirect
 	golang.org/x/sys v0.1.0 // indirect
--- internal/cluster/cluster.go
+++ internal/cluster/cluster.go
@@ -1,105 +1,56 @@
 package cluster
 
 import (
-	"sort"
 	"strconv"
 
 	"github.com/amirhnajafiz/distributed-redis/internal/cmd/server"
-	"github.com/amirhnajafiz/distributed-redis/pkg/http_client"
-	"github.com/gin-gonic/gin"
+	"github.com/amirhnajafiz/strago"
 )
-
-// LoadBalancingServer
-// is the server type.
-type LoadBalancingServer interface {
-	Health() interface{}
-	Start(string string) error
-}
-
-// node
-// manages to keep information about a service.
-type node struct {
-	// service ip.
-	ip string
-	// number of times that the service is used.
-	used int
-}
-
-// cluster
-// manages to monitor clusters and do the load balancing logic.
-type cluster struct {
-	// Capacity is the maximum number of replicas.
-	capacity int
-	// HttpClient manages the requests in cluster.
-	httpClient *http_client.HTTPClient
-	// the array of nodes.
-	nodes []*node
-}
 
 // create
 // manages to create internal services.
-func (c *cluster) create() {
-	port := 12489
+func create(capacity int) []string {
+	var (
+		list []string
 
-	for i := 0; i < c.capacity; i++ {
+		port = 12489
+	)
+
+	for i := 0; i < capacity; i++ {
 		address := ":" + strconv.Itoa(port)
-		n := node{
-			ip:   "localhost" + address,
-			used: 0,
-		}
+		n := "localhost" + address
 
 		port++
 
-		c.nodes = append(c.nodes, &n)
+		list = append(list, n)
 
 		go server.New(address)
 	}
-}
 
-// getIP
-// returns an ip based of the load balancing logic.
-func (c *cluster) getIP() string {
-	n := c.nodes[0]
-
-	n.used++
-
-	sort.Slice(c.nodes, func(i, j int) bool {
-		return c.nodes[i].used < c.nodes[j].used
-	})
-
-	return n.ip
+	return list
 }
 
 // New
 // creating a new cluster.
-func New(capacity int) LoadBalancingServer {
-	c := cluster{
-		capacity:   capacity,
-		httpClient: http_client.New(),
+func New(capacity, addr int) strago.LoadBalancer {
+	nodes := create(capacity)
+
+	opt := strago.NewOptions()
+
+	opt.Port = addr
+	opt.Enable = true
+	opt.BalancingType = strago.RequestsCount
+	opt.Type = "http"
+
+	app := strago.NewServer(opt)
+
+	var services []string
+
+	for _, serv := range nodes {
+		services = append(services, serv)
 	}
 
-	c.create()
+	app.WithServices(services...)
 
-	return &c
-}
-
-// Start
-// starting the cluster.
-func (c *cluster) Start(addr string) error {
-	app := gin.Default()
-
-	gin.SetMode(gin.ReleaseMode)
-	app.Use(c.handle)
-
-	if err := app.Run(addr); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-// Health
-// returns status about cluster.
-func (c *cluster) Health() interface{} {
-	return c.capacity
+	return app
 }
